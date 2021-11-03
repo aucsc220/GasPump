20507eb9622bb6a458369aa5cd6f3c4065388c22
Heise <rosanna@sts.ad.ualberta.ca>
2021-11-02 22:21:15 -0600

    Button methods now public, highlight buttons according to what is pressed, and bound to gas price buttons

--- app/src/main/java/ca/ualberta/rosanna/gaspump/MainActivity.java
+++ app/src/main/java/ca/ualberta/rosanna/gaspump/MainActivity.java
@@ -2,8 +2,10 @@ package ca.ualberta.rosanna.gaspump;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,31 +23,54 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Sets up the instance fields for regular gas.
-     * @param regButton - the button the customer pressed
+     * @param regButtonV - the button the customer pressed
      * Side-effect: changes an instance variable
      */
-    private void regGas(View regButton){
+    public void regGas(View regButtonV){
+        //The three gas types interact (future: consider radio button)
+        //Find the 3 buttons
+        Button regButton = (Button) regButtonV;
+        Button midGradeButton = (Button) findViewById(R.id.midGradeButton);
+        Button premiumButton = (Button) findViewById(R.id.premiumButton);
+
+        regButton.setBackgroundColor(getResources().getColor(R.color.purple_500));
+        midGradeButton.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        premiumButton.setBackgroundColor(getResources().getColor(R.color.purple_200));
+
         costPerLitre = REG_COST_P_L;
-        //TODO Grey out the other two kinds of gas
     }//regGas
 
     /**
      * Sets up the instance fields for mid-grade gas.
-     * @param midGradeButton - the button the customer pressed
+     * @param midGradeButtonV - the button the customer pressed
      * Side-effect: changes an instance variable
      */
-    private void midGradeGas(View midGradeButton){
+    public void midGradeGas(View midGradeButtonV){
+        Button midButton = (Button) midGradeButtonV;
+        Button regButton = (Button) findViewById(R.id.regularButton);
+        Button premiumButton = (Button) findViewById(R.id.premiumButton);
+
+        midButton.setBackgroundColor(getResources().getColor(R.color.purple_500));
+        regButton.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        premiumButton.setBackgroundColor(getResources().getColor(R.color.purple_200));
+
         costPerLitre = MIDGRADE_COST_P_L;
-        //TODO Grey out the other two kinds of gas
     }//midGradeGas
 
     /**
      * Sets up the instance fields for premium gas.
-     * @param premiumButton - the button the customer pressed
+     * @param premiumButtonV - the button the customer pressed
      * Side-effect: changes an instance variable
      */
-    private void premiumGas(View premiumButton){
+    public void premiumGas(View premiumButtonV){
+        Button premiumButton = (Button) premiumButtonV;
+        Button regButton = (Button) findViewById(R.id.regularButton);
+        Button midGradeButton = (Button) findViewById(R.id.midGradeButton);
+
+        premiumButton.setBackgroundColor(getResources().getColor(R.color.purple_500));
+        regButton.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        midGradeButton.setBackgroundColor(getResources().getColor(R.color.purple_200));
+
         costPerLitre = PREMIUM_COST_P_L;
-        //TODO Grey out the other two kinds of gas
     }//midGradeGas
 }
