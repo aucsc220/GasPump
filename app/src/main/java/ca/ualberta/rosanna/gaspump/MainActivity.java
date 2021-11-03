f9bf0580d015420256010a4e8d81f4d3c27d4b7f
Heise <rosanna@sts.ad.ualberta.ca>
2021-11-02 21:57:35 -0600

    Set up instance variables and stubs for when gas type selected

--- app/src/main/java/ca/ualberta/rosanna/gaspump/MainActivity.java
+++ app/src/main/java/ca/ualberta/rosanna/gaspump/MainActivity.java
@@ -3,12 +3,49 @@ package ca.ualberta.rosanna.gaspump;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private double REG_COST_P_L = 139.9;
+    private double MIDGRADE_COST_P_L = 144.9;
+    private double PREMIUM_COST_P_L = 149.9;
+    private double costPerLitre = 0.0; //Nothing selected
+    private int numLitres = 0; //Nothing pumped
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Sets up the instance fields for regular gas.
+     * @param regButton - the button the customer pressed
+     * Side-effect: changes an instance variable
+     */
+    private void regGas(View regButton){
+        costPerLitre = REG_COST_P_L;
+        //TODO Grey out the other two kinds of gas
+    }//regGas
+
+    /**
+     * Sets up the instance fields for mid-grade gas.
+     * @param midGradeButton - the button the customer pressed
+     * Side-effect: changes an instance variable
+     */
+    private void midGradeGas(View midGradeButton){
+        costPerLitre = MIDGRADE_COST_P_L;
+        //TODO Grey out the other two kinds of gas
+    }//midGradeGas
+
+    /**
+     * Sets up the instance fields for premium gas.
+     * @param premiumButton - the button the customer pressed
+     * Side-effect: changes an instance variable
+     */
+    private void premiumGas(View premiumButton){
+        costPerLitre = PREMIUM_COST_P_L;
+        //TODO Grey out the other two kinds of gas
+    }//midGradeGas
 }
