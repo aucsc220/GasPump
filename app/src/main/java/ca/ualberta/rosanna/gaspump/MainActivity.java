4a29a71dcfca2b4f77ca9e5df13f69c99f7f6487
Heise <rosanna@sts.ad.ualberta.ca>
2021-11-03 11:48:04 -0600

    Slider for gas filling now works; BUG when gas type changed

--- app/src/main/java/ca/ualberta/rosanna/gaspump/MainActivity.java
+++ app/src/main/java/ca/ualberta/rosanna/gaspump/MainActivity.java
@@ -6,19 +6,51 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private double REG_COST_P_L = 139.9;
     private double MIDGRADE_COST_P_L = 144.9;
     private double PREMIUM_COST_P_L = 149.9;
-    private double costPerLitre = 0.0; //Nothing selected
+    private boolean gasTypeSelected = false; //Start with no gas type selected
+    private double costPerLitre = 00.0; //Nothing selected yet so don't know cost at start
     private int numLitres = 0; //Nothing pumped
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set up the filling up bar
+        SeekBar fillTankSB = (SeekBar) findViewById(R.id.tankFillingSeekBar);
+        fillTankSB.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if (gasTypeSelected) {
+                            TextView actLitTV = (TextView) findViewById(R.id.actLitresTV);
+                            actLitTV.setText("" + progress + ".0");
+                            TextView actCostTV = (TextView) findViewById(R.id.actCostTV);
+                            actCostTV.setText(Double.toString(progress * costPerLitre / 100.0));
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Pick a type of gas",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+                }
+        );
     }
 
     /**
@@ -38,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
         premiumButton.setBackgroundColor(getResources().getColor(R.color.purple_200));
 
         costPerLitre = REG_COST_P_L;
+        gasTypeSelected = true;
     }//regGas
 
     /**
@@ -55,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         premiumButton.setBackgroundColor(getResources().getColor(R.color.purple_200));
 
         costPerLitre = MIDGRADE_COST_P_L;
+        gasTypeSelected = true;
     }//midGradeGas
 
     /**
@@ -72,5 +106,6 @@ public class MainActivity extends AppCompatActivity {
         midGradeButton.setBackgroundColor(getResources().getColor(R.color.purple_200));
 
         costPerLitre = PREMIUM_COST_P_L;
+        gasTypeSelected = true;
     }//midGradeGas
 }
